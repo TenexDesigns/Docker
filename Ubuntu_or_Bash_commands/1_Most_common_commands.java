9b9ada18e0a9df2c4524e77d0a235b03014e6d3f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-31 10:58:58 +0300

    Create 1_Most_common_commands.java

---
+++ Ubuntu_or_Bash_commands/1_Most_common_commands.java
@@ -0,0 +1,74 @@
+Ubuntu Linux provides a wide range of commands that you can use in its terminal to perform various tasks. These commands are similar to the ones found in other Linux distributions. Here's a list of commonly used commands in Ubuntu and Linux in general:
+
+1. **Navigation and File Management**:
+   - `ls`: List files and directories.
+   - `cd`: Change directory.
+   - `pwd`: Print working directory.
+   - `mkdir`: Create a directory.
+   - `cp`: Copy files or directories.
+   - `mv`: Move or rename files and directories.
+   - `rm`: Remove files and directories.
+   - `find`: Search for files and directories.
+
+2. **Text Manipulation**:
+   - `cat`: Display file content.
+   - `grep`: Search for patterns in files.
+   - `sed`: Stream editor for text manipulation.
+   - `awk`: Text processing tool.
+   - `echo`: Print text to the terminal.
+
+3. **File Permissions**:
+   - `chmod`: Change file permissions.
+   - `chown`: Change file ownership.
+   - `chgrp`: Change group ownership.
+
+4. **System Information**:
+   - `uname`: Print system information.
+   - `df`: Display disk space usage.
+   - `free`: Display memory usage.
+   - `top`: Display system information and processes.
+   - `ps`: Display information about running processes.
+
+5. **Networking**:
+   - `ifconfig` or `ip`: Display network interface configuration.
+   - `ping`: Send ICMP echo requests to a host.
+   - `nslookup` or `dig`: DNS-related commands.
+   - `netstat`: Display network statistics.
+   - `ssh`: Securely connect to remote systems.
+
+6. **Package Management**:
+   - `apt-get`: Command-line package manager (Ubuntu/Debian).
+   - `yum` or `dnf`: Command-line package managers (CentOS/RHEL).
+
+7. **User and Group Management**:
+   - `adduser` or `useradd`: Add a user.
+   - `passwd`: Change user password.
+   - `usermod`: Modify user attributes.
+   - `groupadd`: Add a group.
+   - `groupmod`: Modify group attributes.
+
+8. **Process Management**:
+   - `ps`: Display process status.
+   - `top` or `htop`: Interactive process viewer.
+   - `kill` or `pkill`: Terminate processes.
+   - `jobs` and `fg`: Manage background jobs.
+
+9. **Shell Scripting**:
+   - `bash`: The default shell in Ubuntu.
+   - `sh`: The Bourne shell.
+   - `chmod +x script.sh`: Make a script executable.
+   - `./script.sh`: Run a shell script.
+
+The Ubuntu console (terminal) provides a shell interface where you can enter commands to interact with the system. The default shell in Ubuntu is usually the Bash shell (Bourne Again Shell). Bash is both a command-line interpreter (CLI) and a scripting language. When you run commands in the terminal, you're essentially using the Bash shell to execute those commands.
+
+Bash scripting involves writing scripts that contain sequences of Bash commands and control structures, allowing you to automate tasks, perform system administration, and more. So, while the commands you use in the terminal are the same as those in Bash scripting, scripting extends the capabilities by allowing you to write scripts that execute multiple commands in a sequence.
+
+
+
+
+
+
+
+
+
+  
